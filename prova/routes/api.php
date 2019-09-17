5264859e2d0d2f64c605e5695f2383a975f7817d
gabriel <gfiamoncini@unidavi.edu.br>
2019-09-17 20:20:01 -0300

    ..

--- prova/routes/api.php
+++ prova/routes/api.php
@@ -19,3 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('contas', 'ContaController@index');
 Route::get('contas', 'ContaController@show');
+Route::get('contas', 'ContaController@store');
+Route::get('contas', 'ContaController@destroy');
